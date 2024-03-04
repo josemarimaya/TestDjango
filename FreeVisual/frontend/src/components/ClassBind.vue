78dc66059e5c0f95b38cca0f1ba2bdfcaef5aff0
josemarimaya <josemarimaya@gmail.com>
2024-03-04 11:15:19 +0100

    Learning basic directives

---
+++ FreeVisual/frontend/src/components/ClassBind.vue
@@ -0,0 +1,19 @@
+<template>
+    <!--<p v-bind:class="{'danger': hasError}"> El mensaje se ha enviado correctamente</p>-->
+    <p v-bind:style="{backgroundColor : '#ccc', color:c}"> El mensaje se ha enviado correctamente</p>
+    <button v-on:click="handleClick()"> Cambiar color</button>
+</template>
+
+<script setup>
+   
+   import {ref} from 'vue'
+
+   let c = ref('#fff')
+   const handleClick = () => {c.value = '#ff0000'}
+</script>
+
+<style scoped>
+.danger{
+    color: red;
+}
+</style>
